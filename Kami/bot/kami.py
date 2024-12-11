59e51f2dfe167f64a286eab62967f55aa4603833
Manfred Fong <mafong081102@gmail.com>
2024-12-11 01:20:30 -0500

    created bot on discord admin and connected it to codebase

--- Kami/bot/kami.py
+++ Kami/bot/kami.py
@@ -1,9 +1,22 @@
-import discord
+import discord # type: ignore
 import asyncio
-from discord.ext import commands
-class Kami(discord.client):
-    client = commands.Bot(command_prefix='-')
+import os
+from discord.ext import commands # type: ignore
+from dotenv import load_dotenv # type: ignore 
+
+#Loading the token
+load_dotenv()
+DiscordToken = os.getenv("discordbotToken")
+
+#Primary Bot function
+class Kami(discord.Client):
     #Making sure the bot joins successfully
-    @client.event
-    async def on_ready():
+    async def on_ready(self):
         print('Bot is ready!')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Kami(intents=intents)
+client.run(DiscordToken)
