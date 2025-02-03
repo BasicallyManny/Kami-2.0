191bdf282b7bc4f1f5eeb4432ceff18dceb38c15
Manfred Fong <mafong081102@gmail.com>
2025-02-03 17:04:15 -0500

    implemented lifecycle function for fastAPI. Connects to Mongo on Start up and disconnects on shutdown. renamed and reorganized routes

--- Kami/backend/routes/coordinateRoutes.py
+++ Kami/backend/routes/coordinateRoutes.py
@@ -3,24 +3,16 @@ from models.coordinates import MinecraftCoordinate
 from schema.schemas import coordinates_serializer
 from config.connections import MongoConnection
 from typing import Dict, Any, List
-from dotenv import load_dotenv
 from fastapi import APIRouter, HTTPException # type: ignore
 from datetime import datetime
 
 
 
-# Load environment variables
-load_dotenv()
-
-# Initialize mongoDB connection
-mongoConnection = MongoConnection("mongoConnectionString")
-mongoConnection.connect()
-
 # Create fastAPI router
 router = APIRouter()
-
 DB_NAME = "minecraft_db"
 COLLECTION_NAME = "coordinates"
 
-# GET Request Method
+# ROUTE METHODS
+
 
